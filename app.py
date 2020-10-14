d47716886e1692da2060e25715e424fea1777e4c
Alena Shapovalova <ashapovalova@networkoptix.com>
2020-10-14 09:22:28 +0300

    Flask app with authorization

---
+++ app.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from project import create_app
+
+app = create_app()
+
+if __name__ == "__main__":
+    app.run()
---
+++ project/models.py
@@ -0,0 +1,26 @@
+from . import db
+from flask_login import UserMixin
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True) # primary keys are required by SQLAlchemy
+    email = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(100))
+    name = db.Column(db.String(1000))
+    authenticated = db.Column(db.Boolean, default=True)
+
+
+    def is_active(self):
+        """True, as all users are active."""
+        return True
+
+    def get_id(self):
+        """Return the email address to satisfy Flask-Login's requirements."""
+        return self.id
+
+    def is_authenticated(self):
+        """Return True if the user is authenticated."""
+        return self.authenticated
+
+    def is_anonymous(self):
+        """False, as anonymous users aren't supported."""
+        return False
