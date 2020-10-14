8d8028b605472701b0d9f0691940ebb1503d4e54
Alena Shapovalova <ashapovalova@networkoptix.com>
2020-10-14 23:59:21 +0300

    Added session and records tables

--- project/models.py
+++ project/models.py
@@ -27,3 +27,16 @@ class User(db.Model):
 
     def get_session(self):
         return self.current_record
+
+class Sessions(db.Model):
+    id = db.Column(db.Integer, primary_key=True) # primary keys are required by SQLAlchemy
+    user = db.Column(db.Integer, db.ForeignKey(User.id))
+    session = db.Column(db.String(30), unique=True, default=None)
+
+class Records(db.Model):
+    id = db.Column(db.Integer, primary_key=True) # primary keys are required by SQLAlchemy
+    user = db.Column(db.Integer, db.ForeignKey(User.id))
+    started = db.Column(db.DateTime)
+    stopped = db.Column(db.DateTime)
+    expire_date = db.Column(db.DateTime)
+
